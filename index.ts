689f201bf1b034ccb0b42ad0e09d3de6c08fb0e0
miwtoo <miwyugioh@gmail.com>
2025-01-11 22:34:53 +0700

    refactor: Hands-On : Decorator #1

--- index.ts
+++ index.ts
@@ -9,17 +9,27 @@ interface CurrencyResult<T extends string> {
 //     'USDTHB': 35
 // }
 
-class Currency<const Currencies> {
+function cache(method: Function, context: unknown) {
+	return function a(from: string, to: string, amount: number) {
+		method(from, to, amount);
+	};
+}
+
+class Currency<
+	const Currencies extends readonly string[],
+	Values extends string = Extract<Currencies[keyof Currencies], string>,
+> {
 	api = "https://api.frankfurter.app";
 	private caches: Record<string, number> = {};
 	constructor(public currencies: Currencies) {}
 
-	async convert<To extends Currencies[keyof Currencies]>(
+	// @cache
+	async convert<To extends Values>(
 		from: Currencies[keyof Currencies],
 		to: To,
 		amount: number,
 	) {
-		const key = `${from}${to}` as To extends string ? To : never;
+		const key = `${from}${to}`;
 
 		if (this.caches[key]) {
 			return this.caches[key] * amount;
@@ -28,13 +38,10 @@ class Currency<const Currencies> {
 		const result = await fetch(
 			`${this.api}/latest?from=${from}&to=${to}&amount=${amount}`,
 		)
-			.then(
-				(x) =>
-					x.json() as any as CurrencyResult<To extends string ? To : never>,
-			)
+			.then((x) => x.json() as unknown as CurrencyResult<To>)
 			.then((a) => a);
 
-		const rate = result.rates[to as To extends string ? To : never];
+		const rate = result.rates[to as To];
 		this.caches[key] = rate / result.amount;
 
 		return rate;
@@ -55,7 +62,7 @@ const a = async () => {
 	await myCurrency.convert("USD", "THB", 1).then(console.log);
 	await myCurrency.convert("USD", "THB", 2).then(console.log);
 	await myCurrency.convert("USD", "THB", 3).then(console.log);
-	await myCurrency.convert("USD", "THB", 4).then(console.log);
+	await myCurrency.convert("JPY", "THB", 2).then(console.log);
 };
 
 a();
