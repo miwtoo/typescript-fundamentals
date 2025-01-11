79be6812ce8d65ddcbeff48eccd53aa4abad33a2
miwtoo <miwyugioh@gmail.com>
2025-01-11 22:15:10 +0700

    refactor: Hands-On : Caches

--- index.ts
+++ index.ts
@@ -1,34 +1,66 @@
-interface CurrencyResult {
-    amount: number
-    base: string
-    date: string
-    rates: {
-        [a: string]: number
-    }
+interface CurrencyResult<T extends string> {
+	amount: number;
+	base: string;
+	date: string;
+	rates: Record<T, number>;
 }
+
+// const caches = {
+//     'USDTHB': 35
+// }
 
 class Currency<const Currencies> {
-    api = 'https://api.frankfurter.app'
-    constructor(public currencies: Currencies) { }
+	api = "https://api.frankfurter.app";
+	private caches: Record<string, number> = {};
+	constructor(public currencies: Currencies) {}
 
-    convert(from: Currencies[keyof Currencies], to: Currencies[keyof Currencies], amount: number) {
-        return fetch(`${this.api}/latest?from=${from}&to=${to}&amount=${amount}`)
-            .then((x) => x.json() as any as CurrencyResult)
-            .then((a) => a)
-    }
+	async convert<To extends Currencies[keyof Currencies]>(
+		from: Currencies[keyof Currencies],
+		to: To,
+		amount: number,
+	) {
+		const key = `${from}${to}` as To extends string ? To : never;
 
-    get latest() {
-        return fetch(`${this.api}/latest`).then((x) => x.json())
-    }
+		if (this.caches[key]) {
+			return this.caches[key] * amount;
+		}
 
-    log() {
-        return this.currencies;
-    }
+		const result = await fetch(
+			`${this.api}/latest?from=${from}&to=${to}&amount=${amount}`,
+		)
+			.then(
+				(x) =>
+					x.json() as any as CurrencyResult<To extends string ? To : never>,
+			)
+			.then((a) => a);
+
+		const rate = result.rates[to as To extends string ? To : never];
+		this.caches[key] = rate / result.amount;
+
+		return rate;
+	}
+
+	get latest() {
+		return fetch(`${this.api}/latest`).then((x) => x.json());
+	}
+
+	log() {
+		return this.currencies;
+	}
 }
 
-const myCurrency = new Currency(['USD', 'JPY', 'THB']);
-myCurrency.convert('USD', 'THB', 1).then(console.log)
-myCurrency.latest.then(console.log)
+const myCurrency = new Currency(["USD", "JPY", "THB"]);
+
+const a = async () => {
+	await myCurrency.convert("USD", "THB", 1).then(console.log);
+	await myCurrency.convert("USD", "THB", 2).then(console.log);
+	await myCurrency.convert("USD", "THB", 3).then(console.log);
+	await myCurrency.convert("USD", "THB", 4).then(console.log);
+};
+
+a();
+
+// myCurrency.latest.then(console.log);
 
 // const a = ['USD', 'JPY', 'THB'] as const
 // type A = typeof a
